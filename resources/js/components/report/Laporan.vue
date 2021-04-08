f2a90bce21114783eb681f4df8ce222bac095b3e
ilyasyasin072 <ilyasyasiin072@github.com>
2021-04-08 14:26:36 +0700

    update report

--- resources/js/components/report/Laporan.vue
+++ resources/js/components/report/Laporan.vue
@@ -1,57 +1,7 @@
 <template>
-    <!-- <div class="hello">
-        <h1>{{ msg }}</h1>
-        <h2>Essential Links</h2>
-        <ul>
-            <li>
-                <a href="https://vuejs.org" target="_blank">Core Docs</a>
-            </li>
-            <li>
-                <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-            </li>
-            <li>
-                <a href="https://gitter.im/vuejs/vue" target="_blank"
-                    >Gitter Chat</a
-                >
-            </li>
-            <li>
-                <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-            </li>
-            <br />
-            <li>
-                <a
-                    href="http://vuejs-templates.github.io/webpack/"
-                    target="_blank"
-                    >Docs for This Template</a
-                >
-            </li>
-        </ul>
-        <h2>Ecosystem</h2>
-        <ul>
-            <li>
-                <a href="http://router.vuejs.org/" target="_blank"
-                    >vue-router</a
-                >
-            </li>
-            <li>
-                <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-            </li>
-            <li>
-                <a href="http://vue-loader.vuejs.org/" target="_blank"
-                    >vue-loader</a
-                >
-            </li>
-            <li>
-                <a href="https://github.com/vuejs/awesome-vue" target="_blank"
-                    >awesome-vue</a
-                >
-            </li>
-        </ul>
-        <button @click="generateReport">Export to PDF</button>
-    </div> -->
     <div>
         <vue-html2pdf
-            :show-layout="true"
+            :show-layout="false"
             :float-layout="true"
             :enable-download="true"
             :preview-modal="true"
@@ -62,6 +12,7 @@
             pdf-format="a4"
             pdf-orientation="landscape"
             pdf-content-width="800px"
+            @beforeDownload="beforeDownload($event)"
             @hasStartedGeneration="hasStartedGeneration()"
             @hasGenerated="hasGenerated($event)"
             ref="html2Pdf"
@@ -137,6 +88,43 @@
     </div>
 </template>
 
+<script>
+import VueHtml2pdf from "vue-html2pdf";
+export default {
+    name: "hello",
+    data() {
+        return {
+            msg: "Welcome to Your Vue.js App"
+        };
+    },
+
+    methods: {
+        /*
+            Generate Report using refs and calling the
+            refs function generatePdf()
+        */
+        generateReport() {
+            this.$refs.html2Pdf.generatePdf();
+        },
+        async beforeDownload ({ html2pdf, options, pdfContent }) {
+            await html2pdf().set(options).from(pdfContent).toPdf().get('pdf').then((pdf) => {
+                const totalPages = pdf.internal.getNumberOfPages()
+                for (let i = 1; i <= totalPages; i++) {
+                    pdf.setPage(i)
+                    pdf.setFontSize(10)
+                    pdf.setTextColor(150)
+                    pdf.text('Page ' + i + ' of ' + totalPages, (pdf.internal.pageSize.getWidth() * 0.88), (pdf.internal.pageSize.getHeight() - 0.3))
+                }
+            }).save()
+        }
+    },
+    components: {
+        VueHtml2pdf
+    }
+};
+</script>
+
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1,
@@ -158,27 +146,3 @@ a {
     color: #7db099;
 }
 </style>
-<script>
-import VueHtml2pdf from "vue-html2pdf";
-export default {
-    name: "hello",
-    data() {
-        return {
-            msg: "Welcome to Your Vue.js App"
-        };
-    },
-
-    methods: {
-        /*
-            Generate Report using refs and calling the
-            refs function generatePdf()
-        */
-        generateReport() {
-            this.$refs.html2Pdf.generatePdf();
-        }
-    },
-    components: {
-        VueHtml2pdf
-    }
-};
-</script>
